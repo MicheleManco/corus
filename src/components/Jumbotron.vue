e46216202bbe8170100354b2c0f85467137c5520
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2022-04-09 18:19:33 +0200

    init jumbo

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="big-container">
-    
+    <div class="container">
+      <div class="row">
+        <div class="col-9 row debug1">
+          1 <br>
+          1
+          <div class="col-12 debug3 d-flex justify-content-end">
+            1.1 <br>
+            sdagbvjsaoighdgikodeng
+          </div>
+        </div>
+        <div class="col-3 debug2">
+          2
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +33,16 @@ export default {
   height: 800px;
   background-repeat: no-repeat;
   background-size: 100%;
+}
+
+.debug1{
+  background-color: red;
+}
+.debug2{
+  background-color: pink;
+}
+
+.debug3{
+  background-color: purple;
 }
 </style>
