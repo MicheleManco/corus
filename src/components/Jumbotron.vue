ae1314017ddbcc183f4099c393572ef0cb518d51
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2022-04-14 09:07:58 +0200

    jumbotron finito

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -3,34 +3,34 @@
     <div class="container">
       <div class="row core d-flex justify-content-center">
 
-        <div class="col-sm-9 col-12 row d-flex ">
+        <div class="col-lg-9 col-12 row d-flex ">
 
           <div class="col-2 d-none d-xl-block"> <!--spaziatore --></div>
 
-          <div class="row col-12 col-xl-10">
+          <div class="row col-12 col-lg-10 container-middle">
             <div class="col-12 d-none d-xl-block spazio-titolo-top"></div>
             <div class="col-12">
-              <div class="main-title"><strong>Corsi di Lingua</strong></div>
-              <div class="main-title"><strong>Cinese e Giapponese</strong></div>
+              <div class="main-title robotoB">Corsi di Lingua</div>
+              <div class="main-title robotoB">Cinese e Giapponese</div>
             </div>
             <div class="col-12 row">
-              <div class="col-4 d-none d-lg-block"></div>
-              <div class="col-lg-8 col-12">
+              <div class="col-3 d-none d-xl-block"></div>
+              <div class="col-lg-9 col-12">
                 <div class="titolo-lista">corsi in aula a Brescia</div>
 
                 <div class="clr-white d-flex">
                   <img src="../assets/check-yellow.png" alt="">
-                  <div class="lista"><strong>2 livelli di apprendimento (A1 e A2)</strong></div>
+                  <div class="lista robotoB row-margin-bottom">2 livelli di apprendimento (A1 e A2)</div>
                 </div>
 
                 <div class="clr-white d-flex">
                   <img src="../assets/check-yellow.png" alt="">
-                  <div class="lista"><strong>docenti qualificati</strong></div>
+                  <div class="lista robotoB row-margin-bottom">docenti qualificati</div>
                 </div>
 
                 <div class="clr-white d-flex">
                   <img src="../assets/check-yellow.png" alt="">
-                  <div class="lista"><strong>attestato di partecipazione</strong> rilasciato al termine del corso</div>
+                  <div class="lista"> <span class=" robotoB"> attestato di partecipazione</span> rilasciato al termine del corso</div>
                 </div>
     
               </div>
@@ -39,8 +39,8 @@
 
           <div class="col-12 d-none d-lg-flex bg-gray align-self-end justify-content-between align-items-center">
             <div>
-              <div class="titoletto-giallo"><strong>PARTECIPA AI CORSI IN AULA</strong></div>
-              <div class="sottotitolo-bianco"><strong>e apprendi la lingua cinese e giapponede da docenti qualificati</strong></div>
+              <div class="titoletto-giallo robotoB">PARTECIPA AI CORSI IN AULA</div>
+              <div class="sottotitolo-bianco robotoB">e apprendi la lingua cinese e giapponede da docenti qualificati</div>
               <div class="white-text">termine iscrizioni 7 Ottobre</div>
             </div>
             <img id="arrow-white" src="../assets/arrow-wh-right.png" alt="">
@@ -54,19 +54,19 @@
           
           <div class="colore-orange head-form d-flex align-items-center"><div class="da_ottobre">da ottobre 2018</div></div>
 
-          <div class="padding-blocco richiedi_info"><strong>Richiedi informazioni</strong> </div>
+          <div class="padding-blocco richiedi_info robotoB">Richiedi informazioni</div>
 
           <label for="validationCustom01" class="form-label padding-blocco">Nome e Cognome *</label>
-          <input type="text" class="no_border_radius form-control margin-blocco" id="validationCustom01" value="" required>
+          <input type="text" class="no_border_radius form-control margin-blocco formH" id="validationCustom01" value="" required>
 
           <div class="padding-blocco">telefono</div>
-          <input class="margin-blocco" type="tel">
+          <input class="margin-blocco formH" type="tel">
 
           <label for="exampleInputEmail1" class="padding-blocco form-label">E-mail *</label>
-          <input class="no_border_radius margin-blocco form-control" id="exampleInputEmail1" required type="email">
+          <input class="no_border_radius margin-blocco form-control formH" id="exampleInputEmail1" required type="email">
 
           <div class="padding-blocco">Corso di interesse</div>
-          <select class="no_border_radius margin-blocco" name="interesse">
+          <select class="no_border_radius margin-blocco formH" name="interesse">
             <option value="Cinese-Base" selected > Cinese Base</option>
             <option value="Cinese-Elementare"> Cinese Elementare</option>
             <option value="Giapponese-Base"> Giapponese Base</option>
@@ -126,6 +126,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.robotoB{
+   font-family: 'Roboto';
+   font-weight: 700;
+}
 label{
   margin-bottom: 0px;
 }
@@ -133,11 +137,15 @@ label{
   font-size: 25px;
   line-height: 25px;
 }
+.row-margin-bottom{
+  margin-bottom: 50px;
+}
 .form-check-label{
   margin-left: 5px;
 }
 #form-container{
   min-height: 670px;
+  padding-bottom: 87px;
 }
 .form-check-label{
     text-decoration: underline;
@@ -155,24 +163,21 @@ label{
 .spazio-titolo-top{
   height: 110px;
 }
-
 .main-title{
   color: white;
   font-size: 60px;
   line-height: 60px;
   margin-left: 124px;
   margin-right: -10px;
-  font-family: 'Roboto Bold';
 }
 .titolo-lista{
   color: white;
   font-size: 27px;
-  margin: 50px 0px 20px 116px;
+  margin: 50px 0px 20px 142px;
 }
 .clr-white{
   color: white;
-  margin-left: 80px;
-  margin-bottom: 30px;
+  margin-left: 110px;
   img{
     margin-right: 10px;
     width: 26px;
@@ -181,12 +186,11 @@ label{
 }
 
 .colore-orange {
-  background-color: #dc8122;
-  width: 240px;
+  background-color: #ff9f19;
 }
 .head-form{
   color: white;
-  width: 240px;
+  width: 100%;
   height: 43px;
   padding: 0 20px;
   .da_ottobre{
@@ -202,9 +206,15 @@ label{
   padding: 0 20px;
   margin-top: 6px;
 }
+textarea{
+  resize: none;
+}
+.formH{
+  height: 32px;
+}
 .margin-blocco{
   margin: 0 20px;
-  width: 200px;
+  width: 230px;
 }
 .richiedi_info{
   font-size: 30px;
@@ -214,7 +224,7 @@ label{
 
 .invia_richiesta{
   height: 85px;
-  width: 240px;
+  width:100%;
   font-size: 19px;
   color: rgb(39, 39, 39);
   display: flex;
@@ -232,7 +242,7 @@ label{
 .bg-light-black{
   background-color: rgba(0, 0, 0, 0.493);
   color: white;
-  width: 240px;
+  width: 270px;
   padding: 0;
 }
 .checkbox-container{
@@ -269,26 +279,41 @@ a{
   color: white;
   font-size: 18px;
 }
+@media all and (max-width: 1200px){
+  .main-title{
+    margin-left: 0px;
+  }
+  .clr-white{
+  margin-left: 0px;
+  }
+  .bg-light-black{
+  width: 250px;
+  padding: 0;
+  }
+  .margin-blocco{
+    margin: 0 20px;
+    width: 210px;
+  }
+  .titolo-lista{
+    margin-left: 0px;
+  }
+}
 
-@media all and (max-width: 992px) {
+@media all and (max-width: 991px) {
 .big-container{
   padding-top: 15px;
   padding-bottom: 20px;
 }
-.invia_richiesta{
-  bottom: -20px;
-}
-.main-title{
-  color: white;
-  font-size: 50px;
-  line-height: 50px;
-  margin-left: 0;
-  margin-right: -40px;
-  font-size: 40px;
-}
-.titolo-lista{
-  margin-left: 0px;
-}
+
+ .bg-light-black{
+  width: 80%;
+  padding: 0;
+  }
+  .margin-blocco{
+    margin: 0 20px;
+    width: 90%;
+  }
+
 .clr-white{
   color: white;
   margin-left: 0px;
@@ -304,6 +329,16 @@ a{
   font-size: 27px;
   margin: 50px 0px 20px;
 }
+}
+@media all and (max-width: 467px){
+.bg-light-black{
+  width: 220px;
+  padding: 0;
+  }
+  .margin-blocco{
+    margin: 0 20px;
+    width: 180px;
+  }
 }
 
 </style>
